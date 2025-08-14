74d1e15131037fe1ba9c49dff17ed18125ffcfb0
avery <98293751+crestofthebeast@users.noreply.github.com>
2025-08-14 16:14:37 +1000

    Two triangles with unique VBOs exercise.

--- learn-opengl/learn_opengl.cpp
+++ learn-opengl/learn_opengl.cpp
@@ -102,26 +102,44 @@ int WinMain()
     // tell opengl how to interpret the vertex data we're sending it
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // define some vertices
-    float vertices[] = {
+    
+    /// define first triangle vertices
+    float first_triangle[] = {
         -0.5f,  0.5f, 0.0f,
         -1.0f, -0.5f, 0.0f,
          0.0f, -0.5f, 0.0f,
+    };
+    // and a VBO for them
+    unsigned int VBO0;
+    glGenBuffers(1, &VBO0);
+    // set up a VAO to hold our attribute settings
+    unsigned int VAO0;
+    glGenVertexArrays(1, &VAO0);
+    // bind the VAO
+    glBindVertexArray(VAO0);
+    // copy array of vertices into the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(first_triangle), first_triangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    /// second triangle vertices
+    float second_triangle[] = {
          0.0f, -0.5f, 0.0f,
          0.5f,  0.5f, 0.0f,
          1.0f, -0.5f, 0.0f,
     };
     // and a VBO for them
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
+    unsigned int VBO1;
+    glGenBuffers(1, &VBO1);
     // set up a VAO to hold our attribute settings
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
+    unsigned int VAO1;
+    glGenVertexArrays(1, &VAO1);
     // bind the VAO
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAO1);
     // copy array of vertices into the VBO
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(second_triangle), second_triangle, GL_STATIC_DRAW);
+    
     // set vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -135,8 +153,10 @@ int WinMain()
 
         // let's draw!
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(VAO0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
         
         glfwSwapBuffers(window);
