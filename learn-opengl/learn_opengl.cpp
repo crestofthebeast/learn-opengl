35d4c4e4bb93343ac9309f5dfba66915adc41d26
avery <98293751+crestofthebeast@users.noreply.github.com>
2025-08-14 16:06:37 +1000

    Two triangles exercise.

--- learn-opengl/learn_opengl.cpp
+++ learn-opengl/learn_opengl.cpp
@@ -104,19 +104,13 @@ int WinMain()
     glEnableVertexAttribArray(0);
     // define some vertices
     float vertices[] = {
+        -0.5f,  0.5f, 0.0f,
+        -1.0f, -0.5f, 0.0f,
+         0.0f, -0.5f, 0.0f,
+         0.0f, -0.5f, 0.0f,
          0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+         1.0f, -0.5f, 0.0f,
     };
-    // create some indices to define where the triangles are
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
-    };
-    // make an EBO
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
     // and a VBO for them
     unsigned int VBO;
     glGenBuffers(1, &VBO);
@@ -128,9 +122,6 @@ int WinMain()
     // copy array of vertices into the VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // push the indices into the EBO
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // set vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -145,7 +136,7 @@ int WinMain()
         // let's draw!
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
         
         glfwSwapBuffers(window);
